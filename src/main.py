ecee4dcace98d77d0088a87d0ef9d0f80e42e4ef
shani <shani.valensi1@gmail.com>
2023-12-28 13:31:50 +0200

    clean code & function descriptions

--- src/main.py
+++ src/main.py
@@ -3,69 +3,81 @@ unary = ['~', '!']
 
 
 def main():
+    """
+    An advance calculator program that input mathematical expression of the user until he enter 'end' and print the
+    result of the expression
+    """
     while 1:
-        try:
-            expression = input_exercise()
-        except ValueError as e:
-            print(e)
-            return
-        if expression == "end":
+        # explanation message
+        print("Calculator:\n\tsupported operators:\n\t+ : addition\n\t- : subtraction\n\t* : multiplication\n\t/ : "
+              "division\n\t^ : power\n\t@ : average\n\t$ : maximum\n\t& : minimum\n\t% : modulo\n\t~ : "
+              "negative\n\t! : factorial\n-To end the program enter: 'end'\n")
+        expression = input("Enter an expression to calculate:\n")
+
+        if expression == 'end':
             break
+
         try:
             print(solve(expression))
         except ValueError as e:
             print(e)
+
     print("closing the program...")
 
 
-def input_exercise():
-    print("Calculator:\n\tsupported operators:\n\t+ : addition\n\t- : subtraction\n\t* : multiplication\n\t/ : "
-          "division\n\t^ : power\n\t@ : average\n\t$ : maximum\n\t& : minimum\n\t% : modulo\n\t~ : "
-          "negative\n\t! : factorial\n-To end the program enter: 'end'\n")
-    exercise = input("Enter an expression to calculate:\n")
-    if exercise == 'end':
-        return "end"
-
-    return exercise
-
-
 def solve(expression):
+    """
+    Solves the mathematical expression in parameters
+    :param expression: A string of a mathematical expression
+    :return: The result of the expression
+    """
+    # a list that till hold the elements of the expression
     stack = []
+
     i = 0
     while i < len(expression):
         num, length = read_number(expression, i)
         stack.append(num)
+
         i += length
+        # if an operator don't appear after the number(end of the expression)
         if i >= len(expression):
             break
+
         current_operator = expression[i]
+
+        # if the character that comes after the number in not an accepted operator
         if current_operator not in operators or current_operator in unary:
             raise ValueError("undefined operator: " + current_operator)
-        # if we already had an operator
-        while len(stack) > 2:
-            last_operator = stack[-2]
-            if last_operator not in operators or last_operator in unary:
-                raise ValueError("undefined operator: " + last_operator)
-            # if the power of the last operator is greater or equal to the power of the current operator
-            if operators[last_operator] >= operators[current_operator]:
-                num2 = stack.pop()
-                op = stack.pop()
-                num1 = stack.pop()
-                stack.append(solve_binary_expression(num1, op, num2))
-            else:
-                break
+
+        # if we there's an operator in the stack and its priority is higher or equal to the priority of the current
+        while len(stack) > 2 and operators[stack[-2]] >= operators[current_operator]:
+            num2 = stack.pop()
+            op = stack.pop()
+            num1 = stack.pop()
+            stack.append(solve_binary_expression(num1, op, num2))
+
         stack.append(current_operator)
+
         i += 1
+
     while len(stack) > 1:
         num2 = stack.pop()
         op = stack.pop()
         num1 = stack.pop()
-        print(stack)
+
         stack.append(solve_binary_expression(num1, op, num2))
-    return stack
+
+    return stack[0]
 
 
 def read_number(expression, i):
+    """
+    Read the number in a string expression at index i.
+    :param expression: A string of a mathematical expression
+    :param i: The index in the array of the first character of the number we want to read
+    :return: First value: the number we read. Second value: the number of character that compose the number in the string
+    """
     base_start = i
     sign = 1
     # check '~' before the operand
@@ -92,7 +104,13 @@ def read_number(expression, i):
 
 
 def solve_binary_expression(num1, op, num2):
-    print("solving:", num1, op, num2)
+    """
+    Solves a binary expression (an expression that contain operands and one operator)
+    :param num1: The first operand of the expression
+    :param op: The operator of the expression
+    :param num2: The second operand of the expression
+    :return: The result of the expression
+    """
     if op == '+':
         return num1 + num2
     elif op == '-':
